69e20e5747aaff1454142d8205a1993a1059b634
thiago21sousa21 <thiago21sousa21@gmail.com>
2024-07-02 22:36:06 -0300

    feat: refactor html in js

--- assets/js/page/home.js
+++ assets/js/page/home.js
@@ -1,12 +1,31 @@
-export function generateQuiz() {
-    const container = document.querySelector(".yourQuizes");
+
+
+const createThereIsNoQuiz = ()=>{
+    const home = document.querySelector(".home");
+
+    if (!home) {
+        console.error("Container element not found!");
+        return;
+    }
+
+    home.innerHTML=`
+            <div id="thereIsNoQuiz" class="flex-column-c containerInPage">
+                <p>Você não criou nenhum <br> quizz ainda :(</p>
+                <div id="createQuizBottom" class="flex-column-cc">Criar Quizz</div>
+            </div>
+        `;
+
+}
+
+const createQuiz = (dad)=>{
+    const container = document.querySelector(dad);
 
     if (!container) {
         console.error("Container element not found!");
         return;
     }
 
-    for (let index = 0; index < 5; index++) {
+    for (let index = 0; index < 1; index++) {
         const newQuiz = document.createElement("div");
         newQuiz.classList.add("quiz");
 
@@ -21,4 +40,30 @@ export function generateQuiz() {
     }
 }
 
+const createContainerQuiz = (title, className)=>{
+    const home = document.querySelector(".home");
 
+    if (!home) {
+        console.error("Container element not found!");
+        return;
+    }
+
+    const containerInPage = document.createElement("div");
+    containerInPage.classList.add("containerInPage");
+    containerInPage.classList.add(className);
+
+    const h3 = document.createElement('h3')
+    h3.textContent = title;
+
+    containerInPage.appendChild(h3);       
+    
+    home.appendChild(containerInPage);
+}
+
+createThereIsNoQuiz();
+
+createContainerQuiz("Seus quizes","yourQuizes");
+createContainerQuiz("Todos os quizes", "allQuizes");
+
+createQuiz(".yourQuizes");
+createQuiz(".allQuizes");
